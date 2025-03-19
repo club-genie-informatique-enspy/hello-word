5d2ca724b23e3c20b79b234a7f1d14b763133706
Nameless0l <wwwmbassiloic@gmail.com>
2025-03-19 11:31:35 +0100

    adding of email verification feature

---
+++ app/Notifications/VerifyEmailApi.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Notifications;
+
+
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\URL;
+
+class CustomVerifyEmailNotification extends VerifyEmail
+{
+    protected function verificationUrl($notifiable)
+    {
+        $url = URL::temporarySignedRoute(
+            'verification.verify',
+            Carbon::now()->addMinutes(Config::get('auth.verification.expire', 60)),
+            [
+                'id' => $notifiable->getKey(),
+                'hash' => sha1($notifiable->getEmailForVerification()),
+                'redirect_to' => config('app.frontend_url').'/email-verified'
+            ]
+        );
+
+        return $url;
+    }
+}
